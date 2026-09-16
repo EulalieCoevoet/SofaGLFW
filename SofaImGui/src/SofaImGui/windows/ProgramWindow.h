ff18f46b617b9b07c76be0d33f308710e1f9238b
EulalieCoevoet <eulalie.coevoet@gmail.com>
2026-09-16 17:17:25 +0200

    fix save program filename

--- SofaImGui/src/SofaImGui/windows/ProgramWindow.h
+++ SofaImGui/src/SofaImGui/windows/ProgramWindow.h
@@ -58,7 +58,7 @@ class SOFAIMGUI_API ProgramWindow : public BaseWindow
     bool importProgram(const std::string& filename);
     void exportProgram(const bool &exportAs = true);
 
-    void setProgramFilename(std::string filename) {m_ws_programFilename=filename;}
+    void saveProgramDirAndFilename(const std::string& filename);
 
    protected:
 
@@ -127,8 +127,6 @@ class SOFAIMGUI_API ProgramWindow : public BaseWindow
     bool addAddActionMenu(std::shared_ptr<models::Track> track, const int &trackIndex, const int &actionIndex);
 
     sofa::Index addTrackMenu(const std::string& menuLabel, const sofa::Index& trackIndex, std::shared_ptr<models::Track> track);
-
-    void saveProgramDirAndFilename(const std::string& filename);
 
     bool isDrivingSimulation() {return drivingWindow == DrivingWindow::PROGRAM;}
     void setTime(const double &time);
